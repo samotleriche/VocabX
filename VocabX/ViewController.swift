9bbe169d52afd73043d395fbe7dc77468b634bb2
Tomas Leriche <samotleriche@gmail.com>
2018-08-30 16:06:21 -0600

    Initial Commit

---
+++ VocabX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VocabX
+//
+//  Created by Tomas Leriche on 8/30/18.
+//  Copyright © 2018 Tomas Leriche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
